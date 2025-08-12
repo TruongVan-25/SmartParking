01348a564e30981ef147462dc58cd9a9995ba064
TruongVan-25 <van.truong.cit23@eiu.edu.vn>
2025-08-12 23:52:17 +0700

    fix slots

--- services/get_slot.php
+++ services/get_slot.php
@@ -1,7 +1,7 @@
 <?php
 include("../php/connectSQL.php");
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
